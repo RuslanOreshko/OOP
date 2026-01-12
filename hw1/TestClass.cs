fd6a30c29ea67044fa2a6ee7a25dc7b43e526352
Ffeiitaan <ruslan2006999@gmail.com>
2026-01-12 21:00:27 +0200

    homework 1

---
+++ hw1/TestClass.cs
@@ -0,0 +1,23 @@
+class UserService
+{
+    public void Register()
+    {
+        // Реєстрація
+    }
+}
+
+class EmailService
+{
+    public void EmailMessage()
+    {
+        // Відправка Email
+    }
+}
+
+class Logger
+{
+    public void Logger()
+    {
+        // Логування
+    }
+}
